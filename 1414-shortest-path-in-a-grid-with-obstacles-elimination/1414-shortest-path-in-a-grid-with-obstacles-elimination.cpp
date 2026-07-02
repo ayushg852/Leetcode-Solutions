1d7268a99e414e9d2630d933156d5ea2a238e784
Ayush Gupta <ayushg1025@iitg.ac.in>
2026-07-02 23:17:52 +0530

    Time: 43 ms (54.07%), Space: 20.8 MB (65.17%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-int m,n;
-queue<vector<int>> q;
-vector<vector<vector<int>>> d;
-void func(int old_i,int old_j,int new_i,int new_j,int old_k,int k,vector<vector<int>>& grid)
-{
-    if(new_i<0||new_i>=m||new_j<0||new_j>=n)
-    return;
-    int new_k=old_k+grid[new_i][new_j];
-    if(new_k>k||d[new_i][new_j][new_k]!=1e8)
-    return;
-    d[new_i][new_j][new_k]=d[old_i][old_j][old_k]+1;
-    q.push({new_i,new_j,new_k});
-}
     int shortestPath(vector<vector<int>>& grid, int k) {
-        m=grid.size();
-        n=grid[0].size();
-        d.assign(m,vector<vector<int>> (n,vector<int>(k+1,1e8))); 
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        vector<vector<vector<bool>>> vis(m,vector<vector<bool>> (n,vector<bool>(k+1,false))); 
+        queue<array<int,3>> q;
         q.push({0,0,0});
-        d[0][0][0]=0;
+        vis[0][0][0]=true;
+        int d=0;
+
+        int dr[]={-1,1,0,0};
+        int dc[]={0,0,-1,1};
+
         while(!q.empty())
         {
-            int old_i=q.front()[0];
-            int old_j=q.front()[1];
-            int old_k=q.front()[2];
-            q.pop();
-            func(old_i,old_j,old_i-1,old_j,old_k,k,grid);
-            func(old_i,old_j,old_i+1,old_j,old_k,k,grid);
-            func(old_i,old_j,old_i,old_j-1,old_k,k,grid);
-            func(old_i,old_j,old_i,old_j+1,old_k,k,grid);
+            int sz=q.size();
+            while(sz--)
+            {
+                auto [r,c,used]=q.front();
+                q.pop();
+
+                if(r==m-1 && c==n-1)
+                    return d;
+                
+                for(int i=0;i<4;i++)
+                {
+                    int nr=r+dr[i];
+                    int nc=c+dc[i];
+                    if(nr<0 || nr>=m || nc<0 || nc>=n)
+                        continue;
+
+                    int nk=used+grid[nr][nc];
+                    if(nk>k || vis[nr][nc][nk])
+                        continue;
+
+                    vis[nr][nc][nk]=true;
+                    q.push({nr,nc,nk});
+                }
+            }
+            d++;
         }
-        int ans=1e8;
-        for(int i=0;i<=k;i++)
-        ans=min(ans,d[m-1][n-1][i]);
-        if(ans==1e8)
         return -1;
-        return ans;
     }
 };
