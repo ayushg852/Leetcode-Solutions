f775da50572c9c7ffeb119a06070ef4135a032c8
Ayush Gupta <ayushg1025@iitg.ac.in>
2026-07-08 12:39:35 +0530

    Time: 22 ms (95.34%), Space: 140 MB (93.26%) - LeetHub

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,39 @@
+using ll=long long;
+const int MOD=1e9+7;
+const int N=100001;
+ll pow10[N];
+int init=[](){
+    pow10[0]=1;
+    for (int i=1; i < N; ++i) {
+        pow10[i]=(pow10[i - 1] * 10) % MOD;
+    }
+    return 0;
+}();
+class Solution {
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n=s.size();
+        vector<int> sum(n+1, 0);
+        vector<ll> x(n+1, 0);
+        vector<int> cnt(n+1, 0);
+        for (int i=0;i<n;++i)
+        {
+            int d=s[i]-'0';
+            sum[i+1]=sum[i]+d;
+            x[i+1] = (d>0) ? (x[i]*10+d) % MOD : x[i];
+            cnt[i+1]=cnt[i]+(d>0);
+        }
+        int m=queries.size();
+        vector<int> res(m,0);
+        for(int i=0;i<m;++i)
+        {
+            int l=queries[i][0];
+            int r=queries[i][1]+1;
+            int length=cnt[r]-cnt[l];
+            ll val_x=(x[r]-x[l] * pow10[length] % MOD+MOD) % MOD;
+            ll val_sum=sum[r] - sum[l];
+            res[i]=(val_x * val_sum) % MOD;
+        }
+        return res;
+    }
+};
