5d95ef8dbdffedb1557622c04f12da197634d29f
Ayush Gupta <ayushg1025@iitg.ac.in>
2026-08-06 23:44:07 +0530

    Time: 0 ms (100%), Space: 10.7 MB (24.33%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int func(int i,vector<int> &nums,vector<int> &dp)
+    {
+        if(i<0)
+        return 0;
+        if(dp[i]!=-1)
+        return dp[i];
+        return dp[i]=max(func(i-1,nums,dp),nums[i]+func(i-2,nums,dp));
+    }
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(),-1);
+        return func(nums.size()-1,nums,dp);
+    }
+};
